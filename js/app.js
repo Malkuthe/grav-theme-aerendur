e7b10a57ed922774e6b420bb565893e0508a9eeb
Malkuthe <jp.baltao@gmail.com>
2016-08-14 09:30:57 -0500

    Improved customization system, added background blur, started basic styling.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
+function squarize() {
+  $('.squarize').each(function() {
+    $(this).css('height', $(this).width() );
+  });
+}
+
+function scrollBlur() {
+  var scroll = $(window).scrollTop();
+  var opacityValue = (scroll / 800);
+
+  $('.blurred-image').css('opacity',opacityValue);
+}
+
+$(window).scroll(function(e){
+  scrollBlur();
+});
+
+$(window).resize(function(e){
+  squarize();
+});
+
+$(document).ready(function(e){
+  squarize();
+});
 
 $(document).foundation();
